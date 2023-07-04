8f063358ffcac6c158a06680ff1c45d7a61b81dc
i539307 <amit.lasri@sap.com>
2023-07-04 23:02:59 +0300

    photos and page3

--- page3/page3-ourLegends.js
+++ page3/page3-ourLegends.js
@@ -28,3 +28,4 @@ function loadFile(event, img) {
 	var img = document.getElementById(img);
 	img.src = URL.createObjectURL(event.target.files[0]);
 };
+
--- page3/page3-ourLegends.php
+++ page3/page3-ourLegends.php
@@ -1,7 +1,7 @@
 <?php
 
     //$server_name = "localhost";
-    $server_name = "asafjonathonop.mtacloud.co.il";
+    $server_name = "localhost";
     $user_name = "asafjona_legend";
     $password = "123456";
     $database_name = "asafjona_our_legends";
@@ -20,12 +20,16 @@
     $lastName = $_POST["lastName"];
     $birthDate = $_POST["birthDate"];
     $deathDate = $_POST["deathDate"];
+    $summary = $_POST["summary"];
     $burialPlace = $_POST["burialPlace"];
+    $burial1 = $_FILES["burial1"]["name"];
+    $burial2 = $_FILES["burial2"]["name"];
+    $burial3 = $_FILES["burial3"]["name"];
     $lifeStory = $_POST["lifeStory"];
-    $photo = $_POST["photo"];
+    $photo = $_FILES["photo"]["name"];
     
-    $sql = "INSERT INTO legends (firstName, lastName, birthDate, deathDate, burialPlace, lifeStory, photo ) 
-            VALUES ('$firstName', '$lastName', '$birthDate', '$deathDate', '$burialPlace', '$lifeStory', '$photo')";
+    $sql = "INSERT INTO legends (firstName, lastName, birthDate, deathDate, summary,  burialPlace, burial1, burial2, burial3, lifeStory, photo ) 
+            VALUES ('$firstName', '$lastName', '$birthDate', '$deathDate', '$summary', '$burialPlace', '$burial1', '$burial2', '$burial3', '$lifeStory', '$photo')";
     if ($conn->query($sql)==FALSE){
         echo "Can not add new legend. Error is: ".$conn->error;
         exit();
