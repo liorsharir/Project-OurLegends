98cc59e2f2a8efb2a8884f98bee46913291e528e
i539307 <amit.lasri@sap.com>
2023-07-05 01:59:24 +0300

    shlishi-night-amit

---
+++ add-new-page/add-new-page.js
@@ -0,0 +1,59 @@
+// Save local changes to local storage, prevent data lost while refresh\reload page.
+window.onload = function(){
+    var firstName = localStorage.getItem('firstName');
+    var lastName = localStorage.getItem('lastName');
+    var birthDate = localStorage.getItem('birthDate');
+    var deathDate = localStorage.getItem('deathDate');
+    var burialPlace = localStorage.getItem('burialPlace');
+    var lifeStory = localStorage.getItem('lifeStory');
+    var profile = localStorage.getItem('profile');
+    if (firstName !== null) $('#firstName').val(firstName); if (lastName !== null) $('#lastName').val(lastName);
+    if (birthDate !== null) $('#birthDate').val(birthDate); if (deathDate !== null) $('#deathDate').val(deathDate);
+    if (burialPlace !== null) $('#burialPlace').val(burialPlace); if (lifeStory !== null) $('#lifeStory').val(lifeStory);
+    if (profile !== null) $('#profile').val(profile);
+}
+
+window.onbeforeunload = function() {
+    localStorage.setItem("firstName", $('#firstName').val());
+    localStorage.setItem("lastName", $('#lastName').val());   
+    localStorage.setItem("birthDate", $('#birthDate').val());   
+    localStorage.setItem("deathDate", $('#deathDate').val());
+    localStorage.setItem("burialPlace", $('#burialPlace').val());
+    localStorage.setItem("lifeStory", $('#lifeStory').val());
+    localStorage.setItem("profile", $('#profile').val());
+}
+
+// Replace img icon with uploaded picture
+function loadFile(event, img) {
+	var img = document.getElementById(img);
+	img.src = URL.createObjectURL(event.target.files[0]);
+};
+
+function validateForm(){
+    var birthDate = document.forms["legendsForm"]["birthDate"].value;
+    var currentDate= new Date();
+    var entereBirthDate= new Date(birthDate);
+    if(entereBirthDate > currentDate){
+        window.alert("The birthDate value cannot be in the future");
+        return false;
+    }
+    var deathDate = document.forms["legendsForm"]["deathDate"].value;
+    var entereDeathDate= new Date(deathDate);
+    if(entereDeathDate > currentDate){
+        window.alert("The deathDate value cannot be in the future");
+        return false;
+    }
+    return true;
+}
+
+function validateImage(fileInput){
+    var file = fileInput.files[0];
+    var maxSizeInBytes = 16777215; // 16.7 MB
+
+    if (file.size > maxSizeInBytes) {
+      window.alert("File size exceeds the maximum allowed limit (1 MB). Please choose a smaller image.");
+      fileInput.value = ''; // Clear the file input field
+      return false;
+    }
+    return true;
+}
---
+++ add-new-page/add-new-page.php
@@ -0,0 +1,41 @@
+<?php
+
+    //$server_name = "localhost";
+    $server_name = "localhost";
+    $user_name = "asafjona_legend";
+    $password = "123456";
+    $database_name = "asafjona_our_legends";
+
+    //create connection
+    $conn = new mysqli($server_name,$user_name,$password,$database_name);
+    mysqli_set_charset($conn, "utf8");
+
+    //check the connection
+    if ($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
+    }
+
+    //get info from html
+    $firstName = $_POST["firstName"];
+    $lastName = $_POST["lastName"];
+    $birthDate = $_POST["birthDate"];
+    $deathDate = $_POST["deathDate"];
+    $summary = $_POST["summary"];
+    $burialPlace = $_POST["burialPlace"];
+    $burial1 = $_FILES["burial1"]["name"];
+    $burial2 = $_FILES["burial2"]["name"];
+    $burial3 = $_FILES["burial3"]["name"];
+    $lifeStory = $_POST["lifeStory"];
+    $photo = $_FILES["photo"]["name"];
+    
+    $sql = "INSERT INTO legends (firstName, lastName, birthDate, deathDate, summary,  burialPlace, burial1, burial2, burial3, lifeStory, photo ) 
+            VALUES ('$firstName', '$lastName', '$birthDate', '$deathDate', '$summary', '$burialPlace', '$burial1', '$burial2', '$burial3', '$lifeStory', '$photo')";
+    if ($conn->query($sql)==FALSE){
+        echo "Can not add new legend. Error is: ".$conn->error;
+        exit();
+    }
+    
+    // Close the database connection
+    $conn->close();
+
+?>
