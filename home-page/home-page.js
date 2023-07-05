3925b5e2f54c17ed3010d9f97e1f1e02761eaaec
Lior Sharir <lior.sharir@sap.com>
2023-07-05 18:23:44 +0300

    fix footer

--- home-page/home-page.js
+++ home-page/home-page.js
@@ -1,7 +1,3 @@
-var firstName = localStorage.getItem('firstName');
-var lastName = localStorage.getItem('lastName');
-window.alert("Page for: "+firstName+" "+lastName+ " has been created. \n you can now choose the page you want to view from the list");
-
 document.getElementById("search").addEventListener('click', function() {
     alert('החיפוש לא עובד כאשר הדף לא מחובר לdatabase ');
 });
