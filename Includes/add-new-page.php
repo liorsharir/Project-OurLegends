8ca0e395a30e65a9fbadfd90fc400c9f43a5e91a
Lior Sharir <lior.sharir@sap.com>
2023-07-05 21:39:54 +0300

    fix folders

--- Includes/add-new-page.php
+++ Includes/add-new-page.php
@@ -38,7 +38,7 @@
     // Close the database connection
     $conn->close();
 
-    header('Location: ../personal-memorial-page/personal-memorial-page.html');
+    header('Location: personal-memorial-page.html');
     exit();
 
 ?>
